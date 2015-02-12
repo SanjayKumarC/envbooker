9463f083417a3caa26e703938944ffab6f9f3093
John Griffiths <gnuchu@Johns-MacBook-Pro.local>
2015-02-12 14:47:58 +0000

    Tidy up scaffolded input screen 0 why? they'll get blasted later?

--- Gemfile
+++ Gemfile
@@ -27,6 +27,8 @@ gem "twitter-bootstrap-rails"
 gem "less-rails"
 gem "therubyracer"
 
+gem 'jquery-turbolinks'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/models/envapp.rb
+++ app/models/envapp.rb
@@ -1,4 +1,9 @@
 class Envapp < ActiveRecord::Base
   belongs_to :env
   belongs_to :app
+
+  validates :env, presence: true
+  validates :app, presence: true
+  validates_uniqueness_of :env
+  validates_uniqueness_of :app
 end
