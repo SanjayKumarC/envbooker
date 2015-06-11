5b8ebbcb1386358d1434bbb6b21376a7d64d7b92
John Griffiths <gnuchu@gmail.com>
2015-06-11 16:56:37 +0100

    ssl setting

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :application, 'envbooker'
 set :repo_url, 'git@bitbucket.org:gnuchu/envbooker.git'
 set :deploy_to, '/home/deploy/envbooker'
 set :keep_releases, 5
-set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml .rbenv-vars}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 namespace :deploy do
