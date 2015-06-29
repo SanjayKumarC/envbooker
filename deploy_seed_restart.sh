66b2c5d888ebc489a4d58268057378d93b47a5eb
John Griffiths <gnuchu@gmail.com>
2015-06-29 09:53:19 +0100

    Fixes

--- deploy_seed_restart.sh
+++ deploy_seed_restart.sh
@@ -2,5 +2,6 @@ git commit -am "Fixes"
 git push -u origin --all
 git push -u origin --tags
 bin/cap production deploy
+bin/cap production rails:rake:db:migrate
 bin/cap production rails:rake:db:seed
 bin/cap production deploy:restart
