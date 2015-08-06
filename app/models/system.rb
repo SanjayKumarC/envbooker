143ea074e72050d7528bf8d4a5a9721efedb1a23
John Griffiths <gnuchu@gmail.com>
2015-08-06 15:49:02 +0100

    system model key validation

--- app/models/system.rb
+++ app/models/system.rb
@@ -2,7 +2,8 @@ class System < ActiveRecord::Base
   belongs_to :env
   belongs_to :app
 
+  validates_uniqueness_of :env, scope: :app
+
   has_many :system_properties
   accepts_nested_attributes_for :system_properties, reject_if: proc { |sp| sp['key'].blank? }, allow_destroy: true
-
 end
