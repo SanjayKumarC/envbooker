ea23a2da82f16732f8ea23dfb345ae299058297c
John Griffiths <gnuchu@gmail.com>
2015-08-07 15:47:39 +0100

    fuck ie.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -122,10 +122,10 @@ class HomeController < ApplicationController
 
 	  #color, color, 2nd, 2nd, color, color, 2nd, 2nd
 	  str = sprintf("color: %7s; \
+background: %7s; \
 background: -webkit-repeating-linear-gradient(45deg, %7s, %7s 10px, %7s 10px, %7s 20px); \
 background: -ms-repeating-linear-gradient(45deg, %7s, %7s 10px, %7s 10px, %7s 20px); \
-background: repeating-linear-gradient(45deg, %7s, %7s 10px, %7s 10px, %7s 20px); \
-filter: progid:DXImageTransform.Microsoft.gradient(-45deg, %7s, %7s 10px, %7s 10px, %7s 20px);", tc,c,c,sec,sec,c,c,sec,sec,c,c,sec,sec,c,c,sec,sec)
+background: repeating-linear-gradient(45deg, %7s, %7s 10px, %7s 10px, %7s 20px);", tc,c,c,c,sec,sec,c,c,sec,sec,c,c,sec,sec,c,c,sec,sec)
 	  return str
 	end
 
--- deploy_seed_restart.sh
+++ deploy_seed_restart.sh
@@ -3,6 +3,6 @@ git push -u origin --all
 git push -u origin --tags
 bin/cap production deploy:backup
 bin/cap production deploy
-# bin/cap production rails:rake:db:migrate
-# bin/cap production rails:rake:db:seed
+bin/cap production rails:rake:db:migrate
+bin/cap production rails:rake:db:seed
 bin/cap production deploy:restart
