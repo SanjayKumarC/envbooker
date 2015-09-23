41f62ca5526e03fcb0a49d5203007210637ba49c
gnuchu <gnuchu@gmail.com>
2015-09-23 12:05:32 +0100

    remove unused tools and vendor/cache

--- tools/schemdump.sh
+++
@@ -1,1 +0,0 @@
-java -jar ./schemaSpy_5.0.0.jar -t pgsql -db development_envbooker -host localhost -p 5432 -u rails -p admin -o ./schemaspy -dp ~/Downloads/postgresql-9.4-1201.jdbc4.jar -s public -noads
